d09e54a3a173bc5f16ad8215631ca08a23e72ccc
Allan Arnesen <allan.arnesen@gmail.com>
2016-10-06 16:25:42 +0200

    More cleanup

--- Oblig_2_H2016/src/GenericMergeSort/GenericMergeSort.java
+++ Oblig_2_H2016/src/GenericMergeSort/GenericMergeSort.java
@@ -1,7 +1,4 @@
 package GenericMergeSort;
-
-import java.util.ArrayList;
-
 public class GenericMergeSort {
 	public static <E extends Comparable<E>> void mergeSort(E[] list){
 		if(list.length > 1){
@@ -10,19 +7,16 @@ public class GenericMergeSort {
 			mergeSort(firsthalf);
 			
 			E[] secondhalf = (E[]) new  Comparable[list.length / 2];
-			System.arraycopy(list, list.length / 2, secondhalf, 0, (list.length - (list.length / 2 )));
+			System.arraycopy(list, list.length - (list.length / 2 ), secondhalf, 0, (list.length - (list.length / 2 )));
 			mergeSort(secondhalf);
 			
 			merge(firsthalf, secondhalf, list);
 		}
-
-	}
-	
+	}	
 	public static <E> void merge(E[] firsthalf, E[] secondhalf, E[] temp){
 		int current1 = 0;
 		int current2 = 0;
 		int current3 = 0;
-		
 		while(current1 < firsthalf.length && current2 < secondhalf.length){
 			if(((Comparable) firsthalf[current1]).compareTo(secondhalf[current2]) < 0){
 				temp[current3++] = firsthalf[current1++];
@@ -37,13 +31,5 @@ public class GenericMergeSort {
 		while(current2 < secondhalf.length){
 			temp[current3++] = secondhalf[current2++];
 		}
-	}
-	
-	
-	
-	
-	// Just a test case to see if we imported the right packages
-	public static String print(){
-		return "it works!";
 	}
 }
