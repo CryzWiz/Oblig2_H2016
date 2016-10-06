430399338ecc78f214900a711a825cf105242130
Allan Arnesen <allan.arnesen@gmail.com>
2016-10-06 14:01:48 +0200

    Added more code to GenericMergeSort.

--- Oblig_2_H2016/src/GenericMergeSort/GenericMergeSort.java
+++ Oblig_2_H2016/src/GenericMergeSort/GenericMergeSort.java
@@ -1,5 +1,9 @@
 package GenericMergeSort;
 
+
+
+
+
 public class GenericMergeSort {
 	public static <E extends Comparable<E>> void mergeSort(E[] list){
 		if(list.length > 1){
@@ -17,7 +21,24 @@ public class GenericMergeSort {
 	}
 	
 	public static <E> void merge(E[] firsthalf, E[] secondhalf, E[] temp){
+		int current1 = 0;
+		int current2 = 0;
+		int current3 = 0;
 		
+		while(current1 < firsthalf.length && current2 < secondhalf.length){
+			if(((Comparable) firsthalf[current1]).compareTo(secondhalf[current2]) < 0){
+				temp[current3++] = firsthalf[current1++];
+			}
+			else {
+				temp[current3++] = secondhalf[current2++];
+			}
+		}
+		while(current1 < firsthalf.length){
+			temp[current3++] = firsthalf[current1++];
+		}
+		while(current2 < secondhalf.length){
+			temp[current3++] = secondhalf[current2++];
+		}
 	}
 	
 	
